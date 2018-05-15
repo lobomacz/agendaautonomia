a5421598cdfbaa15dfdc31eb0f08f6321a5d2827
Marvin Córdoba <lobomacz@gmail.com>
2018-05-15 10:38:16 -0600

    Antes de cambio de pc

--- src/app/contactos/nuevo/nuevo.component.ts
+++ src/app/contactos/nuevo/nuevo.component.ts
@@ -13,10 +13,30 @@ import { Funcionario } from '../../clases/funcionario';
 export class NuevoComponent implements OnInit {
 
 	private usuario:any;
+	private funcionario:Funcionario;
 
   constructor(_service:AgendaService) { }
 
   ngOnInit() {
+  	this.funcionario = new Funcionario();
+  	this.funcionario.nombre = '';
+  	this.funcionario.organizacion = '';
+  	this.funcionario.cargo = '';
+  	this.funcionario.region = '';
+  	this.funcionario.municipio = '';
+  	this.funcionario.correo = '';
+  	this.funcionario.telefono = '';
+  	this.funcionario.movil = '';
+  }
+
+  OnFotoChange(foto:any) {
+  	console.log('archivo');
+  	console.log(foto.value);
+  	console.log(foto.files[0].type);
+  }
+
+  OnGuardar() {
+
   }
 
 }
